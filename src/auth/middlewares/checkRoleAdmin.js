2792579f41a858ca26471cd2a525d5d195e52ee1
rubsuadav <rsuarezdavid@gmail.com>
2024-04-18 14:47:54 +0200

    chore: added the middelware to protected the route and now admis can only create users

--- src/auth/middlewares/checkRoleAdmin.js
+++ src/auth/middlewares/checkRoleAdmin.js
@@ -1,8 +1,31 @@
-import { getIdTokenResult } from "firebase/auth";
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDocs, query, collection, where } from "firebase/firestore";
+import admin from "firebase-admin";
+import fs from "fs";
+import { Buffer } from "buffer";
 
 // local imports
-import { FIREBASE_AUTH, FIREBASE_DB } from "../../../firebaseConfig.js";
+import { FIREBASE_DB } from "../../../firebaseConfig.js";
+import { getUserRole } from "../utils/utils.js";
+
+// External imports
+import { config } from "dotenv";
+
+config();
+
+const credentialsBase64 = process.env.GOOGLE_APPLICATION_CREDENTIALS_BASE64;
+const credentialsBytes = Buffer.from(credentialsBase64, "base64");
+
+if (!fs.existsSync("credentials.json")) {
+  fs.writeFileSync("credentials.json", credentialsBytes);
+}
+
+if (admin) {
+  admin.initializeApp({
+    credential: admin.credential.cert(
+      JSON.parse(fs.readFileSync("credentials.json"))
+    ),
+  });
+}
 
 export const checkAdmin = async (req, res, next) => {
   const token = req.headers.authorization;
@@ -12,15 +35,20 @@ export const checkAdmin = async (req, res, next) => {
   }
 
   try {
-    const decodedToken = await getIdTokenResult(
-      FIREBASE_AUTH.currentUser,
-      true
+    const decodedToken = await admin.auth().verifyIdToken(token);
+
+    const userQuery = query(
+      collection(FIREBASE_DB, "users"),
+      where("email", "==", decodedToken.email)
     );
 
-    const userRef = doc(FIREBASE_DB, "users", decodedToken.uid);
-    const userSnap = await getDoc(userRef);
+    const userQuerySnapshot = await getDocs(userQuery);
+    const userSnap = userQuerySnapshot.docs[0];
 
-    if (!userSnap.exists() || userSnap.data().role !== "admin") {
+    const userRef = doc(FIREBASE_DB, "users", userSnap.id);
+    const roleName = await getUserRole(userRef);
+
+    if (roleName !== "admin") {
       return res.status(403).json({ error: "Not an admin" });
     }
 
--- src/auth/routes/createUser.routes.js
+++ src/auth/routes/createUser.routes.js
@@ -2,10 +2,10 @@ import { Router } from "express";
 
 //local imports
 import { createUser } from "../controllers/createUser.controller.js";
-// import { checkAdmin } from "../middlewares/checkRoleAdmin.js";
+import { checkAdmin } from "../middlewares/checkRoleAdmin.js";
 
 const router = Router();
 
-router.post("/create",  createUser);
+router.post("/create", checkAdmin, createUser);
 
 export default router;
