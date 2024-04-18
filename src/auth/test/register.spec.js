1c0e05b37ab431a72e63667dd52eeb0a8489b8fa
rubsuadav <rsuarezdavid@gmail.com>
2024-04-19 01:24:34 +0200

    test: added negative test cases register users

--- src/auth/test/register.spec.js
+++ src/auth/test/register.spec.js
@@ -3,17 +3,18 @@ import request from "supertest";
 // local imports
 import app from "../../app";
 import { generatePhoneNumber } from "./utils.js";
+import testCases from './testNegativeCases.js';
 
-describe("register tests", () => {
-  const data = {
-    name: "Test",
-    lastName: "User",
-    email: "testuser3@gmail.com",
-    password: "@Password1234",
-    phone: "696981567",
-    birthDate: "10/07/1998",
-  };
+const data = {
+  name: "Test",
+  lastName: "User",
+  email: "testuser3@gmail.com",
+  password: "@Password1234",
+  phone: "696981567",
+  birthDate: "10/07/1998",
+};
 
+describe("Can register user", () => {
   beforeEach(async () => {
     await request(app).post("/auth/register").send(data);
   });
@@ -39,11 +40,15 @@ describe("register tests", () => {
     expect(res.statusCode).toEqual(201);
     expect(res.body).toHaveProperty("token");
   });
+});
 
-  it("should fail when the email is already in use", async () => {
-    const res = await request(app).post("/auth/register").send(data);
-
-    expect(res.statusCode).toEqual(400);
-    expect(res.body).toHaveProperty("email", "Email already in use");
+// Negative test cases
+describe("Can't register user", () => {
+  testCases.forEach(({ description, data, expectedStatusCode, expectedProperty, expectedErrorMessage }) => {
+    it(description, async () => {
+      const res = await request(app).post("/auth/register").send(data);
+      expect(res.statusCode).toEqual(expectedStatusCode);
+      expect(res.body).toHaveProperty(expectedProperty, expectedErrorMessage);
+    });
   });
 });
