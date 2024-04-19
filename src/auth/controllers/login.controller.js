11b44c1457c4b2532b51c782bbacd641f7b5d92e
rubsuadav <rsuarezdavid@gmail.com>
2024-04-19 20:51:11 +0200

    refactor: cleaning unnecesary code

--- src/auth/controllers/login.controller.js
+++ src/auth/controllers/login.controller.js
@@ -19,17 +19,8 @@ export const login = async (req, res) => {
     res.status(200).json({ token });
   } catch (error) {
     switch (error.code) {
-      case "auth/invalid-credential":
-        res.status(400).json({ email: "Invalid credentials" });
-        break;
-      case "auth/user-disabled":
-        res.status(400).json({ email: "The user is disabled" });
-        break;
       case "auth/user-not-found":
         res.status(400).json({ email: "User not found" });
-        break;
-      case "auth/wrong-password":
-        res.status(400).json({ password: "Wrong password" });
         break;
     }
   }
