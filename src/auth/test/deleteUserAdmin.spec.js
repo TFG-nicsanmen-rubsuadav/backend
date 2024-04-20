4d7b83a69cc64ff5b692364b9dbca829bffb6a59
rubsuadav <rsuarezdavid@gmail.com>
2024-04-20 20:16:08 +0200

    test: solving test error

--- src/auth/test/deleteUserAdmin.spec.js
+++ src/auth/test/deleteUserAdmin.spec.js
@@ -6,14 +6,14 @@ import { doc, setDoc } from "firebase/firestore";
 // local imports
 import app from "../../app";
 import { FIREBASE_AUTH, FIREBASE_DB } from "../../../firebaseConfig.js";
-import mainUser from "./mainUserForMiddelware.js";
+import { mainUser2, mainUser3 } from "./mainUserForMiddelware.js";
 import { populateRoles } from "../utils/utils.js";
 
 let uid;
 let userToken;
 
 beforeAll(async () => {
-  const response = await request(app).post("/auth/register").send(mainUser);
+  const response = await request(app).post("/auth/register").send(mainUser2);
   uid = response.body.uid;
   const customToken = await admin.auth().createCustomToken(uid);
   const userCredentials = await signInWithCustomToken(
@@ -25,7 +25,6 @@ beforeAll(async () => {
 
 describe("Controlling user deletion with admin middleware throwing invalids tokens", () => {
   it("no token provider", async () => {
-    console.log(uid);
     const res = await request(app).delete`/auth/delete/${uid}`;
     expect(res.statusCode).toEqual(401);
     expect(res.body).toHaveProperty("error");
@@ -56,17 +55,31 @@ describe("Controlling user deletion with admin middleware throwing valid tokens"
       .delete(`/auth/delete/invalidUid`)
       .set("Authorization", userToken);
     expect(res.statusCode).toEqual(404);
-    expect(res.body).toHaveProperty("error");
+    expect(res.body).toHaveProperty("message");
   });
-  it("valid token and user found", async () => {
+  it("valid token and user found and cant delete because is admin", async () => {
     const rolesIds = await populateRoles();
     const userDoc = doc(FIREBASE_DB, "users", uid);
     const roleDoc = doc(userDoc, "role", rolesIds["admin"]);
     await setDoc(roleDoc, { name: "admin" });
     const res = await request(app)
-      .set("Authorization", userToken)
-      .delete(`/auth/delete/${uid}`);
-    console.log(res.body);
+      .delete(`/auth/delete/${uid}`)
+      .set("Authorization", userToken);
+    expect(res.statusCode).toEqual(401);
+    expect(res.body).toHaveProperty("message");
+  });
+
+  it("valid token and user found and deleted", async () => {
+    const response2 = await request(app).post("/auth/register").send(mainUser3);
+    const uid2 = response2.body.uid;
+    console.log(uid2);
+    const rolesIds = await populateRoles();
+    const userDoc = doc(FIREBASE_DB, "users", uid2);
+    const roleDoc = doc(userDoc, "role", rolesIds["customer"]);
+    await setDoc(roleDoc, { name: "customer" });
+    const res = await request(app)
+      .delete(`/auth/delete/${uid2}`)
+      .set("Authorization", userToken);
     expect(res.statusCode).toEqual(204);
     expect(res.body).toHaveProperty("message");
   });
