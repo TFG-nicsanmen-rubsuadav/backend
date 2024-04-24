88831b98fe02a539b943487699eb07a4dd0234f2
rubsuadav <rsuarezdavid@gmail.com>
2024-04-24 14:29:02 +0200

    fix: solved test positive creation user and initial creation

--- src/auth/test/initialCreation.js
+++ src/auth/test/initialCreation.js
@@ -1,8 +1,9 @@
 export default {
-  name: "Test",
+  name: "Owner",
   lastName: "User",
   email: "testuser3@gmail.com",
   password: "@Password1234",
   phone: "696981567",
   birthDate: "10/07/1998",
+  rol: "owner",
 };
